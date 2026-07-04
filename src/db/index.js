f2fdf6dc25d8b4a09c4a93017a2d259205151216
Manindra-NS <manindraseth3@gmail.com>
2026-07-04 10:09:19 +0530

    fix DNS problem

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+//DNS resolution issue fix
+import dns from"dns";
+dns.setServers(["1.1.1.1","8.8.8.8"]);
+
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
